f92c9e2b5ef2dbdc76741c1d6dbf7a124be6c470
renuy <renu.yarday@strata.co.in>
2011-05-01 23:17:12 +0530

    gateway re-entry

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -30,8 +30,8 @@ class Payment < ActiveRecord::Base
   end
   
   def RedirectUrl
-    "http://192.168.2.100:3000/gatewayentry"
-    #http://kids.justbooksclc.com/gatewayentry"
+    #"http://192.168.2.100:3000/gatewayentry"
+    "http://kids.justbooksclc.com/gatewayentry"
   end
   
   def OrderId
