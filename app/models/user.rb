239633f87276a0d24994d3b6969440d48be0d369
renuy <renu.yarday@strata.co.in>
2011-07-12 12:19:52 +0530

    user confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :registerable, :recoverable,
+  devise :database_authenticatable, :registerable, :recoverable,:confirmable
          #:rememberable, :trackable, 
          :validatable
 
